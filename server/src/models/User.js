151428ba33e4de529476c42228e6d178be20983e
bereket <iberekety@gmail.com>
2023-08-14 18:42:11 -0400

    Update schema for test

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 
 const userScema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: false, unique: true },
   password: { type: String, required: true },
 })
 
