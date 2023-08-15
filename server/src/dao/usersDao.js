870fb80923c273c9ceccd65b25ea54b8857b258d
bereket <iberekety@gmail.com>
2023-08-14 22:46:14 -0400

    Protect password

--- server/src/dao/usersDao.js
+++ server/src/dao/usersDao.js
@@ -4,9 +4,9 @@ const usersDao = {
   createUser: (username, password) => User.create({username, password}),
   updateUser: (uid, updates) => User.updateOne({_id: uid}, {$set: updates}),
   deleteUser: (uid) => User.deleteOne({_id: uid}),
-  findByEmail: (email) => User.findOne({email}),
-  findByUserName: (username) => User.findOne({username}),
-  findByCredentials: (credentials) => User.findOne(credentials)
+  findByEmail: (email) => User.findOne({email}, {password: false}),
+  findByUserName: (username) => User.findOne({username}, {password: false}),
+  findByCredentials: (credentials) => User.findOne(credentials, {password: false})
 }
 
 export default usersDao;
