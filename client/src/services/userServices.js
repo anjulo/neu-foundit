ba84fa914d6bfde77256e8fbc71aec75ca2e80fe
bereket <iberekety@gmail.com>
2023-08-18 10:41:40 -0400

    Move url to .env

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -1,7 +1,6 @@
 import axios from "axios"
 
-// const REACT_APP_BASE_URL = process.env.REACT_APP_BASE_URL;
-const REACT_APP_BASE_URL = "http://localhost:4000";
+const REACT_APP_BASE_URL = process.env.REACT_APP_BASE_URL;
 const api = axios.create({
   withCredentials: true,
 });
